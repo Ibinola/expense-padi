9d1e8c2be709294a022bed9b0366f1316f4d9781
Ibinola <ibinolamichael1@gmail.com>
2024-10-05 11:00:00 +0100

    install dependencies and set up routes

---
+++ src/pages/auth/sign-up/SignUp.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import signUpBackgroundImage from '../../../assets/sign-up-bg-image.svg'
+
+function SignUp() {
+  return (
+    <>
+      {/* <img src={signUpBackgroundImage} />
+      <div className='text-3xl font-bold underline'>SignUp</div> */}
+    </>
+  
+  )
+}
+
+export default SignUp
