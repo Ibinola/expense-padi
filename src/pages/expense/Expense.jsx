19a3563f0c63e184e82521919db313336e2bee5c
Ibinola <ibinolamichael1@gmail.com>
2024-10-12 02:18:04 +0100

    fix unresolved path, and remove lorem ipsum text in the settings page

--- src/pages/expense/Expense.jsx
+++ src/pages/expense/Expense.jsx
@@ -7,7 +7,8 @@ import CustomBarChart from '../../components/BarChart';
 import TransactionTable from '../../components/Table';
 import DonutChart from '../../components/DonutChart';
 // import { FiPlus } from 'react-icons/fi';
-import Example from '../../components/Dropdown';
+// import Example from '../../components/Dropdown';
+// import CustomDropdown from '../../components/CustomDropdown';
 
 function Expense() {
   const options = [
@@ -27,13 +28,13 @@ function Expense() {
           Add Expense
         </button> */}
         {/* <Example /> */}
-        <Example
+        {/* <CustomDropdown
           buttonText="Add Expense"
           options={options}
           // onSelectOption={handleSelectOption}
           menuClass="custom-menu-class"
           buttonClass="custom-button-class"
-        />
+        /> */}
         <button className="flex items-center gap-1 bg-[#0553C7] text-xs px-4 py-2 rounded-md text-white">
           Export report
         </button>
