e5fa6d60884e6c0d9eced98483aeeb2b0f5e97f2
Ibinola <ibinolamichael1@gmail.com>
2024-10-14 09:51:52 +0100

    ui: added bg color to custom btn

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -5,7 +5,7 @@ function CustomButton(props) {
   return (
     <button
       type={type}
-      className="w-full py-4 border rounded-md cursor-pointer"
+      className="w-full bg-[#0557C2] text-white py-4 border rounded-md cursor-pointer"
     >
       {label}
     </button>
