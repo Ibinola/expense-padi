89d072fcd1b1b86faa38d566146cc4011f9dc943
Ibinola <ibinolamichael1@gmail.com>
2024-10-16 03:16:19 +0100

    feat: implement toggle password visibility

--- src/components/CustomInput.jsx
+++ src/components/CustomInput.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ErrorMessage, Field } from 'formik';
 import TextError from './TextError';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 function CustomInput(props) {
   const { type, label, name, placeholder } = props;
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <div className="mt-2 space-y-1">
+    <div className="mt-2 space-y-1 relative">
       <label htmlFor={name} className="text-sm block">
         {label}
       </label>
-      <Field
-        type={type}
-        name={name}
-        placeholder={placeholder}
-        className="w-full p-1 md:p-2 border rounded-md border-[#D0D5DD]"
-      />
+      <div className="flex items-center">
+        <Field
+          type={showPassword ? 'text' : type}
+          name={name}
+          placeholder={placeholder}
+          className="w-full p-1 md:p-2 border rounded-md border-[#D0D5DD]"
+        />
+        {type === 'password' && (
+          <span
+            onClick={togglePasswordVisibility}
+            className="absolute right-3 cursor-pointer text-gray-500"
+          >
+            {showPassword ? <FaEyeSlash /> : <FaEye />}
+          </span>
+        )}
+      </div>
       <ErrorMessage name={name} component={TextError} />
     </div>
   );
